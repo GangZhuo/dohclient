47e7f6e266dce404813bfbca8b9233a18e9f9536
Gang Zhuo <gang.zhuo@gmail.com>
2020-07-08 22:15:14 +0800

    update 'windows\install_service.bat

--- windows/install_service.bat
+++ windows/install_service.bat
@@ -12,12 +12,14 @@ if not exist "%CURR_PATH%%CONFIG_FILE%" (
 		echo.
 		echo config cfg
 		echo 	option bind_addr '127.0.0.1'
-		echo 	option bind_port '1081'
-		echo 	option chnroute '%CURR_PATH%lan.txt,%CURR_PATH%chnroute.txt,%CURR_PATH%chnroute6.txt'
-		echo 	option timeout '30'
+		echo 	option bind_port '53'
+		echo 	option chnroute '%CURR_PATH%chnroute.txt,%CURR_PATH%chnroute6.txt'
+		echo 	option timeout '3'
 		echo 	option log_file '%CURR_PATH%dohclient.log'
 		echo 	option log_level '5'
-		echo 	#option proxy '127.0.0.1:1080'
+		echo 	option proxy '127.0.0.1:1080'
+		echo 	option channel 'doh'
+		echo 	option channel_args 'addr=8.8.4.4:443&host=dns.google&path=/dns-query&proxy=1&ecs=1&china-ip4=114.114.0.0/24&foreign-ip4=8.8.0.0/24'
 	)> "%CURR_PATH%%CONFIG_FILE%"
 )
 
